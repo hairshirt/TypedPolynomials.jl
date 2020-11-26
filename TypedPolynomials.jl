df442eaa15024e64c95cc42325a96d8d1b0cec9f
hairshirt <65773490+hairshirt@users.noreply.github.com>
2020-11-26 03:19:08 -0500

    change default ordering
    groebner basis modifications

---
+++ TypedPolynomials.jl
@@ -0,0 +1,42 @@
+module TypedPolynomials
+
+import MutableArithmetics
+const MA = MutableArithmetics
+
+using Reexport
+@reexport using MultivariatePolynomials
+const MP = MultivariatePolynomials
+
+using DataStructures
+
+using MacroTools
+import Base: *, +, -, /, ^, ==,
+    promote_rule, show, isless, size, getindex,
+    one, zero, iszero, @pure, copy, exponent, vec
+
+using LinearAlgebra
+import LinearAlgebra: dot, adjoint
+export @polyvar,
+    Variable,
+    Monomial,
+    Term,
+    Polynomial,
+    variables,
+    terms,
+    differentiate,
+    subs,
+    MONOMIAL_ORDER
+
+include("sequences.jl")
+import .Sequences: shortest_common_supersequence, mergesorted
+
+include("types.jl")
+include("operators.jl")
+include("substitution.jl")
+include("calculus.jl")
+include("conversion.jl")
+include("promotion.jl")
+include("call.jl")
+include("macros.jl")
+
+end # module
